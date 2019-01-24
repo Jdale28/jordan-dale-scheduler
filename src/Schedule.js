30b8b581c49201bbc667368f261ba53c29941c2b
Jordan Dale <jordan.dale2@gmail.com>
2019-01-24 16:46:07 -0500

    Added counter poll Redux

--- src/Schedule.js
+++ src/Schedule.js
@@ -74,52 +74,75 @@ class Schedule extends Component {
     ]
   };
 
-  activateModal = (e, i) => {
-      let value = e.target.innerHTML;
-      let updatedNewUser = { ...this.state.newUser };
-      if (this.state.availableTimes[i].isTimeBooked === false){
-        updatedNewUser["timeslot"] = value;
-        this.setState({ timeslot: value, newUser: updatedNewUser });
-    } else {
-        let bookedUser = this.state.users.filter(user => {
-            return user.timeslot === value
-        })
-        updatedNewUser = bookedUser[0]
-        this.setState({newUser: updatedNewUser})
+  // When Modal activates, this function runs to determine whether time is available or not, and corresponding actions
+  updateModalFields = (e, i) => {
+    // saves the timeslot selected
+    let value = e.target.innerHTML;
+    let activeUser = { ...this.state.newUser };
+    // IF the time has not been booked, update the timeslot in newUser's information
+    if (this.state.availableTimes[i].isTimeBooked === false) { 
+      activeUser["timeslot"] = value;
+      this.setState({ timeslot: value, newUser: activeUser });
+    } 
+    // ELSE, because time has been booked, populated the booked user's information in the modal input fields
+    else {
+      let bookedUser = this.state.users.filter(user => {
+        return user.timeslot === value;
+      });
+      activeUser = bookedUser[0];
+      this.setState({ newUser: activeUser });
     }
   };
 
+  // Set data from input field into newUser object
   handleInput = e => {
     const updatedNewUser = { ...this.state.newUser };
     updatedNewUser[e.target.name] = e.target.value;
     this.setState({ newUser: updatedNewUser });
   };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     let updatedNewUser = { ...this.state.newUser };
     let allUsers = [...this.state.users];
-    const currentUser = allUsers.filter(user => user.timeslot === updatedNewUser.timeslot)
-    let combinedUsers
-    if (currentUser[0]){
-        const filteredUsers = allUsers.filter(user => user.timeslot !== updatedNewUser.timeslot)
-        combinedUsers = filteredUsers.concat([this.state.newUser])
+    const currentUser = allUsers.filter(
+      user => user.timeslot === updatedNewUser.timeslot
+    );
+    let combinedUsers;
+    if (currentUser[0]) {
+      const filteredUsers = allUsers.filter(
+        user => user.timeslot !== updatedNewUser.timeslot
+      );
+      combinedUsers = filteredUsers.concat([this.state.newUser]);
     } else {
-        combinedUsers = [...this.state.users, this.state.newUser];
+      combinedUsers = [...this.state.users, this.state.newUser];
     }
-
-    let updatedAvailability = [...this.state.availableTimes]
-    const currentTime = updatedAvailability.filter(time => time.time === this.state.timeslot)
+    let updatedAvailability = [...this.state.availableTimes];
+    const currentTime = updatedAvailability.filter(
+      time => time.time === this.state.timeslot
+    );
     const newAvailableTimes = updatedAvailability.map(newTime => {
-        if (newTime.time === currentTime[0].time){
-            newTime.isTimeBooked = true
-        }   
-        return newTime
-    })
-    updatedNewUser["name"] = ""
-    updatedNewUser["number"] = ""
-    updatedNewUser["timeslot"] = ""
-    this.setState({ users: combinedUsers, newUser: updatedNewUser, availableTimes: newAvailableTimes });
+      if (newTime.time === currentTime[0].time) {
+        newTime.isTimeBooked = true;
+      }
+      return newTime;
+    });
+    updatedNewUser["name"] = "";
+    updatedNewUser["number"] = "";
+    updatedNewUser["timeslot"] = "";
+    this.setState({
+      users: combinedUsers,
+      newUser: updatedNewUser,
+      availableTimes: newAvailableTimes
+    });
   };
+
+  loveModals = () => {
+    this.props.dispatch({ type: "LOVE" })
+  }
+
+  hateModals = () => {
+    this.props.dispatch({ type: "HATE" })
+  }
 
   render() {
     const returnTimes = this.state.availableTimes.map((timeslot, i) => {
@@ -127,9 +150,11 @@ class Schedule extends Component {
         <div
           key={i}
           data-toggle="modal"
-          data-target="#exampleModalCenter"
-          onClick={e => this.activateModal(e, i)}
-          className={timeslot.isTimeBooked === true ? "timeslot booked" : "timeslot"}
+          data-target="#modalCenter"
+          onClick={e => this.updateModalFields(e, i)}
+          className={
+            timeslot.isTimeBooked === true ? "timeslot booked" : "timeslot"
+          }
           id={i}
         >
           {timeslot.time}
@@ -143,18 +168,19 @@ class Schedule extends Component {
         </TimeTable>
 
         {/* Modal Code for React-strap */}
+
         <div
           className="modal fade"
-          id="exampleModalCenter"
+          id="modalCenter"
           tabIndex="-1"
           role="dialog"
-          aria-labelledby="exampleModalCenterTitle"
+          aria-labelledby="modalCenterTitle"
           aria-hidden="true"
         >
           <div className="modal-dialog modal-dialog-centered" role="document">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalCenterTitle">
+                <h5 className="modal-title" id="modalCenterTitle">
                   Please enter your name and number to confirm your{" "}
                   {this.state.timeslot} appointment
                 </h5>
@@ -172,7 +198,7 @@ class Schedule extends Component {
               <div className="modal-body">
                 <input
                   onChange={this.handleInput}
-                  value={this.state.newUser.name || "" }
+                  value={this.state.newUser.name || ""}
                   name="name"
                   type="text"
                   placeholder="Your Name"
@@ -201,13 +227,24 @@ class Schedule extends Component {
           </div>
         </div>
         {/* End Modal Code from React-strap */}
+
+        {/* Start of Poll Code */}
+        <div>
+            <h1>Poll:</h1>
+            <h2>Love or Hate Modals?</h2>
+            <div>
+                <button onClick={this.hateModals}>Hate</button>
+                <span className="count">{this.props.count}</span>
+                <button onClick={this.loveModals}>Love</button>
+            </div>
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  timeslot: state.timeslot
+  count: state.count
 });
 
 export default connect(mapStateToProps)(Schedule);
--- src/index.js
+++ src/index.js
@@ -5,17 +5,69 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 const initialState = {
+  count: 0,
   timeslot: "",
-  modals: []
+  users: [],
+  newUser: {
+    name: "",
+    number: "",
+    timeslot: ""
+  },
+  blankUser: {
+    name: "",
+    number: "",
+    timeslot: ""
+  },
+  availableTimes: [
+    {
+      time: "9 a.m.",
+      isTimeBooked: false
+    },
+    {
+      time: "10 a.m.",
+      isTimeBooked: false
+    },
+    {
+      time: "11 a.m.",
+      isTimeBooked: false
+    },
+    {
+      time: "12 a.m.",
+      isTimeBooked: false
+    },
+    {
+      time: "1 p.m.",
+      isTimeBooked: false
+    },
+    {
+      time: "2 p.m.",
+      isTimeBooked: false
+    },
+    {
+      time: "3 p.m.",
+      isTimeBooked: false
+    },
+    {
+      time: "4 p.m.",
+      isTimeBooked: false
+    },
+    {
+      time: "5 p.m.",
+      isTimeBooked: false
+    }
+  ]
 };
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    // case "MODALPOPUP":
-    //   return [...state, {
-    //       timeslot: action.timeslot
-
-    //   }]
+    case "LOVE":
+      return {
+        count: state.count + 1
+      };
+    case "HATE":
+      return {
+        count: state.count - 1
+      };
     default:
       return state;
   }
@@ -27,5 +79,9 @@ const App = () => {
   return <Schedule />;
 };
 
-ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById("root")
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
